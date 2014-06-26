1d6914c3040c5d0aee4c764534ce5239f4f193b7
Harold Dennison <harold.dennison@gmail.com>
2014-06-26 20:34:40 +0200

    First files

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var environments = {
+		'dev': 'app/dev/',
+		'prod': 'app/prod/',
+	},
+
+	assets = {
+		'dev': environments.dev + 'assets/',
+		'prod': environments.prod + 'assets/',
+	},
+
+	development = {
+		'scss': assets.dev + 'scss/',
+		'img': assets.dev + 'img/',
+		'icons': assets.dev + 'img/icons/',
+		'templates': assets.dev + 'templates/',
+	},
+
+	production = {
+		'pages': environments.prod + 'pages/',
+		'css': assets.prod + 'css/',
+		'js':  assets.prod + 'js/',
+		'img': assets.prod + 'img/',
+	};
+
+
+var gulp = require('gulp'),
+	jade = require('gulp-jade'),
+	compass = require('gulp-compass'),
+	svgSprites = require('gulp-svg-sprites');
+
+
+gulp.task('jade',function(){
+	gulp.src(development.templates + '/**/*.jade')
+	.pipe(jade())
+	.pipe(gulp.dest(production.pages));
+});
+
+
+gulp.task('compass', function() {
+	gulp.src(development.scss + '*.scss')
+	.pipe(compass({
+		config_file: 'config.rb',
+		css:  production.css,
+		sass: development.scss
+	}))
+	.pipe(gulp.dest(production.css))
+});
